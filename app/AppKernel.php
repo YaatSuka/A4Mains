c7bf65b0e8c05e8d3f5bcd3bc4f335f333ebaa8e
Tom Chitty <tom@DESKTOP-2512FK7.localdomain>
2017-11-17 15:54:57 +1100

    Nettoyage Repo

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Bmatzner\FontAwesomeBundle\BmatznerFontAwesomeBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new AQM\ArtBundle\AQMArtBundle(),
